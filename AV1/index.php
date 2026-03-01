871149f216325c2d7d8d22a3ab0e11452204c212
Esther Nunes <estherbezerradelima@gmail.com>
2026-03-01 12:44:58 -0300

    Calculadora Web Simples - Projeto

---
+++ AV1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Minha Calculadora</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+<h1>Minha Calculadora</h1>
+
+<form method="POST" action="calculo.php">
+
+1º Número: <input type="number" name="a" required><br><br>
+2º Número: <input type="number" name="b" required><br><br>
+
+    <select name="operacao">
+        <option value="soma">Soma</option>
+        <option value="subtracao">Subtração</option>
+        <option value="multiplicacao">Multiplicação</option>
+        <option value="divisao">Divisão</option>
+    </select>
+
+    <br><br>
+
+    <input type="submit" value="Calcular">
+
+</form>
+
+</body>
+</html>
