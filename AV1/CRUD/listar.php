c0cec8f35d64f8f00ec1e7929794e5cff65a1f07
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 10:47:50 -0300

    Atualizando listar.php para a discursiva

--- AV1/CRUD/listar.php
+++ AV1/CRUD/listar.php
@@ -5,22 +5,35 @@ $arquivo = fopen("dados.txt", "r");
 echo "<h2>Lista de Perguntas</h2>";
 
 while(!feof($arquivo)) {
-$linha = fgets($arquivo);
+    $linha = fgets($arquivo);
 
 f($linha != "") {
+
 $dados = explode(";", $linha);
 
 if(count($dados) >= 3) {
 
-    echo "<b>Pergunta:</b> " . $dados[2] . "<br>";
-if($dados[1] == "M") {
-   echo "A) " . $dados[3] . "<br>";
-  echo "B) " . $dados[4] . "<br>";
-  echo "C) " . $dados[5] . "<br>";
-  echo "D) " . $dados[6] . "<br>";
-  echo "<b>Correta:</b> " . $dados[7] . "<br>";
+cho "<b>ID:</b> " . $dados[0] . "<br>";
+echo "<b>Pergunta:</b> " . $dados[2] . "<br>";
 
-    echo "<hr>";
+
+            if($dados[1] == "M") {
+
+echo "A) " . $dados[3] . "<br>";
+echo "B) " . $dados[4] . "<br>";
+echo "C) " . $dados[5] . "<br>";
+echo "D) " . $dados[6] . "<br>";
+
+echo "<b>Correta:</b> " . $dados[7] . "<br>";
+    }
+
+    
+            if($dados[1] == "D") {
+
+cho "<b>Resposta:</b> " . $dados[3] . "<br>";
+            }
+
+echo "<hr>";
         }
     }
 }
