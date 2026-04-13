690e54775b7edd1c648c53c8bfee4503a9820412
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 10:49:08 -0300

    Update salvar_discursiva.php

--- AV1/CRUD/salvar_discursiva.php
+++ AV1/CRUD/salvar_discursiva.php
@@ -1,1 +1,17 @@
+<?php
 
+$pergunta = $_POST['pergunta'];
+$resposta = $_POST['resposta'];
+
+$id = time();
+
+$arquivo = fopen("dados.txt", "a");
+
+fwrite($arquivo, $id . ";D;" . $pergunta . ";" . $resposta . "\n");
+
+fclose($arquivo);
+
+echo "Sua pergunta discursiva foi salva!<br>";
+echo "<a href='index.php'>Voltar</a>";
+
+?>
