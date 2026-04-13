b81d89e09296797a1ee0e73456916b0809464269
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 10:49:28 -0300

    Update salvar_multipla.php

--- AV1/CRUD/salvar_multipla.php
+++ AV1/CRUD/salvar_multipla.php
@@ -1,1 +1,20 @@
+<?php
 
+$pergunta = $_POST['pergunta'];
+
+$a = $_POST['a'];
+$b = $_POST['b'];
+$c = $_POST['c'];
+$d = $_POST['d'];
+$correta = $_POST['correta'];
+
+$id = time();
+
+$arquivo = fopen("dados.txt", "a");
+
+fwrite($arquivo, $id . ";M;" . $pergunta . ";" . $a . ";" . $b . ";" . $c . ";" . $d . ";" . $correta . "\n");
+fclose($arquivo);
+echo "Sua pergunta foi salva!<br>";
+echo "<a href='index.php'>Voltar</a>";
+
+?>
