9d607f3a0816074fe5e9d67692c7426de7d4805e
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 15:10:21 -0300

    organizando criar_multipla.php

--- AV1/CRUD/criar_multipla.php
+++ AV1/CRUD/criar_multipla.php
@@ -2,24 +2,26 @@
 <html>
 <body>
 
-<h2>Criar Pergunta multipla de Escolha</h2>
+<h2>Criar Pergunta - Múltipla Escolha</h2>
 
 <form action="salvar_multipla.php" method="POST">
 
 Pergunta:<br>
-<input type="text" name="pergunta"><br><br>
+<input type="text" name="pergunta" required><br><br>
 
-Opcao A: <input type="text" name="a"><br>
-Opcao B: <input type="text" name="b"><br>
-Opcao C: <input type="text" name="c"><br>
-Opcao D: <input type="text" name="d"><br><br>
+Opção A: <input type="text" name="a"><br>
+Opção B: <input type="text" name="b"><br>
+Opção C: <input type="text" name="c"><br>
+Opção D: <input type="text" name="d"><br><br>
 
-Resposta correta (A/B/C/D):
+Resposta correta (A/B/C/D):<br>
 <input type="text" name="correta"><br><br>
 
 <input type="submit" value="Salvar">
 
 </form>
+
+<br><a href="index.php">Voltar</a>
 
 </body>
 </html>
