1f8d1c6dfc73b1bece067e1e1d8340aac3fc2f33
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 10:52:31 -0300

    Update criar_discursiva.php

--- AV1/CRUD/criar_discursiva.php
+++ AV1/CRUD/criar_discursiva.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Criar Pergunta Discursiva</h2>
+
+<form action="salvar_discursiva.php" method="POST">
+
+Pergunta:<br>
+<input type="text" name="pergunta"><br><br>
+
+Resposta:<br>
+<input type="text" name="resposta"><br><br>
+
+<input type="submit" value="Salvar">
+
+</form>
+
+</body>
+</html>
