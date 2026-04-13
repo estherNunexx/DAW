65bb8e845a3d99e4d6ed10186b509514cbe0e664
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 11:07:33 -0300

    Update cadastrar.php

--- AV1/CRUD/cadastrar.php
+++ AV1/CRUD/cadastrar.php
@@ -1,1 +1,33 @@
+<?php
 
+if ($_POST) {
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$id = time();
+
+$arquivo = fopen("usuario.txt", "a");
+
+fwrite($arquivo, $id . ";" . $email . ";" . $senha . "\n");
+fclose($arquivo);
+echo "<h3>Usuário cadastrado com sucesso!</h3>";
+echo "<a href='index.php'>Ir para o sistema</a>";
+} else {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar Usuário</title>
+</head>
+<body>
+<h2>Cadastrar Usuário</h2>
+<form method="POST">
+Email: <input type="text" name="email"><br><br>
+Senha: <input type="password" name="senha"><br><br>
+<input type="submit" value="Cadastrar">
+</form>
+</body>
+</html>
+
+<?php
+?>
