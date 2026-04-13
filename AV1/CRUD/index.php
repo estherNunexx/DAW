d5d50b060a5e207b61953350f5e9f3747bb3d10c
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 10:08:37 -0300

    Update index.php

--- AV1/CRUD/index.php
+++ AV1/CRUD/index.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Sistemas de Perguntas</title>
+</head>
+<body>
 
+<h2>Menu</h2>
+
+<a href="criar_multipla.php">Criar Pergunta-Multipla Escolha</a><br>
+<a href="listar.php">Listar Perguntas</a><br>
+
+</body>
+</html>
