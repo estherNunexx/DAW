ec7fa7958ff17ad546a403a3a3b37918f458fd53
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-04-13 10:13:57 -0300

    Update criar_multipla.php

--- AV1/CRUD/criar_multipla.php
+++ AV1/CRUD/criar_multipla.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Criar Pergunta multipla de Escolha</h2>
+
+<form action="salvar_multipla.php" method="POST">
+
+Pergunta:<br>
+<input type="text" name="pergunta"><br><br>
+
+Opcao A: <input type="text" name="a"><br>
+Opcao B: <input type="text" name="b"><br>
+Opcao C: <input type="text" name="c"><br>
+Opcao D: <input type="text" name="d"><br><br>
+
+Resposta correta (A/B/C/D):
+<input type="text" name="correta"><br><br>
+
+<input type="submit" value="Salvar">
+
+</form>
+
+</body>
+</html>
