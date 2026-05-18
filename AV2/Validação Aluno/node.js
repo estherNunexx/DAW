5dfafdf01af42a2a1cd82ed58670cbff9d872ae4
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-05-18 10:26:51 -0300

    validação de email e matricula

--- AV2/Validação Aluno/node.js
+++ AV2/Validação Aluno/node.js
@@ -1,1 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+app.post('/validar', (req, res) => {
+    const nome = req.body.nome;
+    const matricula = req.body.matricula;
+    const email = req.body.email;
+
+    //matricula
+    if (isNaN(matricula) || matricula === "") {
+        return res.json({ texto: "A matrícula deve conter apenas números." });
+    }
+
+    //email
+    const temArroba = email.includes("@");
+    const temPontoCom = email.endsWith(".com");
+    const ehMinusculo = (email === email.toLowerCase());
+
+    if (!temArroba || !temPontoCom) {
+        return res.json({ texto: "O Email deve conter @ e terminar em .com" });
+    }
+
+    if (!ehMinusculo) {
+        return res.json({ texto: "O email deve conter apenas letras minúsculas" });
+    }
+
+    res.json({ texto: "Aluno cadastrado com sucesso!"});
+});
+
+app.listen(3000, () => {
+});
