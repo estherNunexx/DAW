dc0fb1deb0d2a5cca4704c91e49cd9712f1875e5
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-06-01 22:55:24 -0300

    Create index.php

---
+++ AV2/AV1 com JS e JSON/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Sistemas de Perguntas</title>
+</head>
+<body>
+
+<h2>Menu</h2>
+
+<a href="criar_multipla.php">Criar Pergunta-Multipla Escolha</a><br>
+<a href="criar_discursiva.php">Criar Pergunta-Discursiva</a><br>
+<a href="listar.php">Listar Perguntas</a><br>
+<a href="buscar.php">Buscar Pergunta por ID</a><br>
+
+</body>
+</html>
