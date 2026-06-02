5c2e952f1d92d054de7d500c08b28e315cac904d
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-06-01 22:53:16 -0300

    Update criar_discursiva.php

--- AV2/AV1 com JS e JSON/criar_discursiva.php
+++ AV2/AV1 com JS e JSON/criar_discursiva.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Criar Pergunta - Discursiva</h2>
+
+<form action="salvar_discursiva.php" method="POST">
+
+Pergunta:<br>
+<input type="text" name="pergunta" required><br><br>
+
+Resposta:<br>
+<input type="text" name="resposta"><br><br>
+
+<input type="submit" value="Salvar">
+
+</form>
+
+<br><a href="index.php">Voltar</a>
+
+</body>
+</html>
