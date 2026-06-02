78d0a36210c8f2669de67725862b187af10041a1
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-06-01 22:53:36 -0300

    Update criar_multipla.php

--- AV2/AV1 com JS e JSON/criar_multipla.php
+++ AV2/AV1 com JS e JSON/criar_multipla.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Criar Pergunta - Múltipla Escolha</h2>
+
+<form action="salvar_multipla.php" method="POST">
+
+Pergunta:<br>
+<input type="text" name="pergunta" required><br><br>
+
+Opção A: <input type="text" name="a"><br>
+Opção B: <input type="text" name="b"><br>
+Opção C: <input type="text" name="c"><br>
+Opção D: <input type="text" name="d"><br><br>
+
+Resposta correta (A/B/C/D):<br>
+<input type="text" name="correta"><br><br>
+
+<input type="submit" value="Salvar">
+
+</form>
+
+<br><a href="index.php">Voltar</a>
+
+</body>
+</html>
