ba0b8e20862dc95bc241d9154faa352828d3544c
Esther Nunesx <159140151+estherNunexx@users.noreply.github.com>
2026-07-13 12:43:05 -0300

    Create pagamento.js

---
+++ AV2/Salão_de_Beleza/js/pagamento.js
@@ -0,0 +1,62 @@
+function mostrarPagamento(){
+
+let forma = document.getElementById("formaPagamento").value;
+
+document.getElementById("pix").style.display = "none";
+document.getElementById("cartao").style.display = "none";
+document.getElementById("dinheiro").style.display = "none";
+
+if(forma == "pix"){
+
+document.getElementById("pix").style.display = "block";
+
+}
+
+if(forma == "cartao"){
+
+document.getElementById("cartao").style.display = "block";
+
+}
+
+if(forma == "dinheiro"){
+
+document.getElementById("dinheiro").style.display = "block";
+
+}
+
+}
+
+function pagarCartao(){
+
+let numero = document.getElementById("numeroCartao").value;
+let titular = document.getElementById("titular").value;
+let validade = document.getElementById("validade").value;
+let cvv = document.getElementById("cvv").value;
+
+if(numero == "" || titular == "" || validade == "" || cvv == ""){
+
+alert("Preencha todos os dados do cartão.");
+
+return;
+
+}
+
+alert("Processando pagamento...");
+
+setTimeout(function(){
+
+alert("Pagamento realizado com sucesso!");
+
+window.location.href = "home.html";
+
+},2000);
+
+}
+
+function confirmarPagamento(){
+
+alert("Pagamento realizado com sucesso!");
+
+window.location.href="home.html";
+
+}
